c427563e7f57205f9ec5c92704483dec0362f52a
aggelos-007 <aggelos007@gmail.com>
2024-06-06 22:45:43 +0300

    yeet the old code so i can rewrite in peace

--- dist/core/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './fastify';
-//# sourceMappingURL=index.d.ts.map
--- dist/routes/usage.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import { RouteOptions } from 'fastify';
-declare const _default: RouteOptions<import("fastify").RawServerDefault, import("http").IncomingMessage, import("http").ServerResponse<import("http").IncomingMessage>, import("fastify").RouteGenericInterface, unknown, import("fastify").FastifySchema, import("fastify").FastifyTypeProviderDefault, import("fastify").FastifyBaseLogger>;
-export default _default;
-//# sourceMappingURL=usage.d.ts.map
